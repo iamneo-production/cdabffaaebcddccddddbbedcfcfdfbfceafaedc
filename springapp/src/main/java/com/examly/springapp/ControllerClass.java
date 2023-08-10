b1743247e695cfd1ec98a9636baea64dad2f2e9c
Iamneo <techadmin@iamneo.ai>
2023-08-10 11:40:05 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/ControllerClass.java
+++ springapp/src/main/java/com/examly/springapp/ControllerClass.java
@@ -1,4 +1,4 @@
-package com.example.;
+package com.examly.springapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +10,7 @@ import java.util.*;
 @RestController
 public class ControllerClass {
     @Autowired
+    
     ServiceClass service;
     @GetMapping("/alltasks")
     public List<ModelTask> getAllTasks()
